60a0352e326d3be9ddb9bbd9092117b426d407b4
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-12-11 18:22:46 -0600

    started task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  // Checkbox change event listener
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    updateAmenities();
+  });
+
+  $('button').on('click', function() {
+    console.log('Button clicked!');
+    // Add your code here
+});
+
+  // Request API status
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    },
+    error: function () {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+  // Function to update the list of selected amenities
+  function updateAmenities() {
+    const amenityNames = Object.values(amenities);
+    $('.amenities h4').text(amenityNames.join(', '));
+  }
+
+  // Request places from the API
+  fetch('http://0.0.0.0:5001/api/v1/places_search/', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+})
+.then(response => response.json())
+.then(data => {
+    // Loop through the result and create article tags
+    const placesSection = document.querySelector('.places');
+    data.forEach(place => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <!-- Your place details here -->
+            <h2>${place.name}</h2>
+            <!-- Add other details you want to display -->
+        `;
+        placesSection.appendChild(article);
+    });
+})
+.catch(error => console.error('Error fetching places:', error));
+
+});
